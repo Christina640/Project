1473732316edeaf0b3ac7d4e7ca43af93af66ad4
79299 <prest1359@gmail.com>
2019-07-06 16:11:24 +0300

    Init

---
+++ src/Saturday.java
@@ -0,0 +1,18 @@
+public class Saturday {
+    public static void main(String[]args){
+        double first= Double.parseDouble()[0];
+        double second = Double.parseDouble()[1];
+        String operation = null; // + - / *
+        double result =
+        if ("plus" .equals(operation)) {
+            result = first + second;
+        }
+        if ("minus".equals(operation)){
+            result = first - second;
+
+        } System.out.println(result);
+
+    }
+
+
+}
