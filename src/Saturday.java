875f14cfc0bdd486051252b767a94c3f23e9bc94
Christina640 <52599294+Christina640@users.noreply.github.com>
2019-07-08 13:28:50 +0300

    Update Saturday.java

--- src/Saturday.java
+++ src/Saturday.java
@@ -1,18 +1,27 @@
-public class Saturday {
-    public static void main(String[]args){
-        double first= Double.parseDouble()[0];
-        double second = Double.parseDouble()[1];
-        String operation = null; // + - / *
-        double result =
-        if ("plus" .equals(operation)) {
-            result = first + second;
+class Saturday {
+    public static void main(String args[]) {
+        int number = 12;
+        String Furniture;
+        if (number == 7 || number == 1 || number == 2)
+        {
+            Furniture = "Кровать";
         }
-        if ("minus".equals(operation)){
-            result = first - second;
-
-        } System.out.println(result);
-
+        else if (number ==3 || number == 4 || number == 5)
+        {
+            Furniture = "Диван";
+        }
+        else if (number == 6 || number == 12 || number == 8)
+        {
+            Furniture = "Стул";
+        }
+        else if (number == 9 || number == 10 || number == 11)
+        {
+            Furniture = "Стол";
+        }
+        else
+        {
+            Furniture = "Bogus Fur";
+        }
+        System.out.println( "Остап Бендер искал " + Furniture + "№ 12");
     }
-
-
 }
